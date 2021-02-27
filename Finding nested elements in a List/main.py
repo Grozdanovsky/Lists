b3a15f89acdb5979d579c2e2e1c90ba40c2322d4
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-27 19:16:00 +0100

    Task 8 done

---
+++ Finding nested elements in a List/main.py
@@ -0,0 +1,18 @@
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+list2 = [[12, 18, 23, 25, 45], [7, 11, 19, 24, 28], [1, 5, 8, 18, 15, 16]]
+list3 = [[item for item in lst if item in list1] for lst in list2]
+print(list3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
