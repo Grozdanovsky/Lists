fe3abc1a78f999ba86f5c759c8faa704ef43f5b5
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-27 17:28:52 +0100

    Task 2 done

--- Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples/main.py
+++ Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples/main.py
@@ -1,5 +1,7 @@
 list1 = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 
 list1.sort(key=lambda item: item[1], reverse=False)
+# The sort works on a way that ITEM is used twice here because on the left hand of the colon it
+# is the name of a parameter and on the right hand side it is being used in the code block to compute something.
 
 print(list1)
