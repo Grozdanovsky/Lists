b152d4828f9f403b1c213a87e14961211eaf5bb7
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-28 16:57:02 +0100

    Task 9 done

--- Write a Python program to remove key values pairs from a list of dictionaries/main.py
+++ Write a Python program to remove key values pairs from a list of dictionaries/main.py
@@ -13,6 +13,16 @@ list2 = [{key: value for key, value in item.items() if key != "Jan"} for item in
 print(list2)
 
 
+list3 =[]
+for item in list1:
+    for k,v in item.items():
+        if k != "Jan":
+            list3.append({k: v})
+
+print(list3)
+
+
+
 
 
 
